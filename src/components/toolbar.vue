1111f0abaa6a19d932d3501e752150994b12c89f
Misaka <oncwnuC6OQ1Rqu2BCB_PqQoI92xY@git.weixin.qq.com>
2023-04-11 17:46:14 +0800

    NewYork Demo: measure distance

---
+++ src/components/toolbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="toolbar">
+    <el-switch
+      v-model="measureDistance"
+      @change="measureDistance ? activeMeasureDistance() : clearMeasureDistance()"
+    ></el-switch>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "toolbar"
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import cesium from 'vite-plugin-cesium'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue() ,cesium()],
+  resolve: {
+    alias: {
+      'assets': '@/assets'
+    }
+  }
+})
