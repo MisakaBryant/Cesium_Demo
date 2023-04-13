b618fcf10bb6ecebce84bd778a55ca0d7d866546
Grosser-Kurfuerst <bai3641@outlook.com>
2023-04-13 22:12:25 +0800

    test master merge

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import MeasureDistance from "./classes/MeasureDistance.js";
 // test git
 // test master rebase
 // test rebase
+// test master merge
 Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmM2U1YjYxYS1lNzczLTRlMjQtODEyYi03MjJmNjQyOTQzOWYiLCJpZCI6MTMwNjk3LCJpYXQiOjE2Nzk4OTg4MTd9.vTMp7xouXgtGhI3yV4rHa86YV1bopfqmVJcrbttFODU"
 
 let viewer
